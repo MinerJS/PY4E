e13030e4d330de8e079af7723025212ee8da79cd
MinerJS <78239249+MinerJS@users.noreply.github.com>
2021-01-30 21:36:35 +0000

    Add files via upload

---
+++ ex3_3.py
@@ -0,0 +1,21 @@
+score = input("Enter Score: ")
+try:
+    s= float(score)
+except:
+    print("Error: Please input value between 0.0 and 1.0")
+    quit()
+if s>1:
+    print("Error: Please input value between 0.0 and 1.0")
+elif s>= 0.9:
+        print("A")
+elif s >= 0.8:
+        print("B")
+elif s >=0.7:
+        print("C")
+elif s >=0.6:
+        print("D")
+elif s <0.6:
+        if s>0:
+            print("F")
+elif s<0:
+    print("Error: Please input value between 0.0 and 1.0")
